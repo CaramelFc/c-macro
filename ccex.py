a420945259e4f86a0f9d4b960c94e58949ee996c
CaramelFc <CaramelFc@outlook.com>
2020-05-04 22:23:49 +0800

    repo init

---
+++ ccex.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import sys
+import re
+
+
+def find_source_root_dir():
+    cwd = Path.cwd()
+    while str(cwd) != '/':
+        git = cwd / ".git"
+        if git.exists():
+            return cwd
+        cwd = cwd.parent
+    raise Exception("Could not find source root dir by .git")
+
+
+
+class SearchInclude():
+    project_source_dir = find_source_root_dir()
+    cwd = Path.cwd()
+    flags = []
+    def __init__(self, file_name, *args):
+        self.file_wd = (self.cwd / file_name).parent
+        self.include_path = [self.file_wd]
+        self.found = {}
+        self.found_path = {str(self.file_wd):"found"}
+        if (self.project_source_dir / ".cc_config").exists():
+            with open(str(self.project_source_dir / ".cc_config"), "r") as fd:
+                lines = fd.readlines()
+                for line in lines:
+                    if re.match("^-I", line) != None:
+                        path = line.replace('\n', '').split('I')[1]
+                        abs_path = self.project_source_dir / path
+                        if str(abs_path) not in self.found_path:
+                            self.found_path[str(abs_path)] = "found"
+                            self.include_path.append(abs_path)
+                    else:
+                        self.flags += " " + line.replace('\n', '')
+        self.get_include_flags(file_name)
+
+    def get_include_flags(self, file_name):
+        self.dfs(file_name)
+        flags = ""
+        for path in self.include_path:
+            flags += " -I" + str(path)
+        for flag in self.flags:
+            flags += flag
+        print(flags)
+
+    def get_include_file_name(self, file_name):
+        ret = []
+        with open(file_name, "r") as fd:
+            lines = fd.readlines()
+            for line in lines:
+                if re.match('#include.*".*"$', line) != None:
+                    ret.append(line.split('"')[1])
+        return ret
+
+
+
+    def dfs(self, file_name):
+        include_file_name = self.get_include_file_name(file_name)
+        if len(include_file_name) == 0:
+            return
+        for include_file in include_file_name:
+            # class: Path
+            include_file_path = self.find_file(include_file)
+            parent_index = len(include_file.split('/')) - 1;
+            if include_file_path == None:
+                continue
+            if str(include_file_path.parents[parent_index]) not in self.found_path:
+                self.found_path[str(include_file_path.parents[parent_index])] = "found"
+                self.include_path.append(include_file_path.parents[parent_index])
+            if str(include_file_path) not in self.found:
+                self.found[str(include_file_path)] = "found"
+                self.dfs(str(include_file_path))
+            # ...
+
+    def find_file(self, file_name):
+        for path in self.include_path:
+            file_path = path / file_name
+            if file_path.exists() :
+                return file_path
+
+        # try to search in project_source_dir
+        ret = self.search_in_project_source_dir(self.project_source_dir, file_name)
+        if ret == None:
+            print("warnning, %s not find, skip it"%(file_name), file = sys.stderr)
+        return ret
+
+
+
+    def search_in_project_source_dir(self, path, file_name):
+        file_path = path / file_name
+        if file_path.exists():
+            return file_path
+        for child in path.iterdir():
+            if child.is_dir():
+                ret = self.search_in_project_source_dir(child, file_name)
+                if ret != None:
+                    return ret
+        return None
+
+
+
+
+if __name__ == "__main__":
+    SearchInclude(sys.argv[1])
+
---
+++ ccstrip.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import re
+import sys
+
+
+class StripInclude():
+    def __init__(self, src_file, intermediate_file):
+        with open(src_file, "r") as fd1, open(intermediate_file, "r") as fd2:
+            src_lines = fd1.readlines()
+            inter_lines = fd2.readlines()
+            index_lines = [1]
+            for index, line in enumerate(src_lines):
+                if re.match("^#include", line):
+                    index_lines.append(index + 1)
+            while len(index_lines) > 0:
+                start = index_lines[0]
+                l = -1
+                r = -1
+                for index, line in enumerate(inter_lines):
+                    if re.match("^#.*" + src_file, line):
+                        if l != -1:
+                            r = index
+                            break
+                        inter_index = int(line.split(' ')[1])
+                        if l == -1:
+                            if inter_index <= start:
+                                l = index
+                            else:
+                                # may deprecate by macro
+                                break;
+                assert(l == -1 and r == -1 or l != -1 and r != -1)
+                if l != -1 and r != -1:
+                    assert(int(inter_lines[l].split(' ')[1]) <= start and int(inter_lines[r].split(' ')[1]) >= start)
+                if l != -1 and r != -1:
+                    del inter_lines[l:r]
+                del index_lines[0]
+            for line in inter_lines:
+                print(line, end = '')
+
+if __name__ == "__main__":
+    StripInclude(sys.argv[1], sys.argv[2])
+
+
+
+
